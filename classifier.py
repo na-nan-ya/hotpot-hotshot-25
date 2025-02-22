9b633520c23f7ba62d15c828447659b63e2fedd1
Ananya <ananyavganesh@gmail.com>
2025-02-22 14:13:21 -0500

    imports loaded, just updating

---
+++ classifier.py
@@ -0,0 +1,13 @@
+import torch
+import spacy 
+import sklearn as sk
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+nlp = spacy.load("en_core_web_sm")
+
+
+#optimiser = optim.Adam()
+
+

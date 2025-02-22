1e961b1c4aed7fb72e23edfd5fbb6c7698f67b45
Ananya <ananyavganesh@gmail.com>
2025-02-22 17:06:00 -0500

    helper function for process_text() - token2vec() added

--- classifier.py
+++ classifier.py
@@ -8,21 +8,28 @@ from torch.utils.data import DataLoader, TensorDataset
 nlp = spacy.load("en_core_web_sm")
 
 # converts raw text data (sentences) to a list of their tokenised forms
-def text2token(file_name: str):
+def process_text(file_name: str):
     # read input file
     with open(file_name, 'r') as file:
         raw_data = file.readlines()
-    tokenised_text = []
-    # tokenise each sentence individually
+    processed_text = []
+    # process each sentence individually
     for sentence in raw_data:
         # convert to lowercase
         lowered = sentence.lower()
         doc = nlp(sentence) # treat sentence as a spaCy object
-        # convert sentence to a list of tokens
+        # convert sentence to a list of tokens and lemmatise before adding to list
         tokens = [token.lemma_ for token in doc if not token.is_punct] # ignore punctuation
-        tokenised_text.append(tokens) # add list of tokens to main list
-    return tokenised_text
+        # convert list of tokens to a list of embedded vectors
+        vectors = token2vector(tokens)
+        processed_text.append(vectors) # add list of vectors to main list
+    return processed_text
 
+# helper method to convert a list of tokens to respective embedded vectors
+def token2vector(tokenList: list):
+    embedded_vector = [token.vector for token in tokenList]
+    return embedded_vector
+    
 
 
 

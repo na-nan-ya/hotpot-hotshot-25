564d72714d1c56a62ef574b186a15eba58fb86cc
Ananya <ananyavganesh@gmail.com>
2025-02-22 15:32:44 -0500

    removed text2vec function, replaced with text2token function - was the wrong approach i think?

--- classifier.py
+++ classifier.py
@@ -7,24 +7,21 @@ from torch.utils.data import DataLoader, TensorDataset
 
 nlp = spacy.load("en_core_web_sm")
 
-# converts raw text data to vector values for training
-def text2vec(file_name: str):
-    # read text file dataset 
+# converts raw text data (sentences) to a list of their tokenised forms
+def text2token(file_name: str):
+    # read input file
     with open(file_name, 'r') as file:
         raw_data = file.readlines()
-    vectorised_text = []
+    tokenised_text = []
+    # tokenise each sentence individually
     for sentence in raw_data:
         # convert to lowercase
         lowered = sentence.lower()
-        # treat sentence as a spaCy object
-        doc = nlp(sentence)
-        # convert tokens in sentence to vector representations (numPy array)
-        vectors = [token.vector for token in doc if not token.is_punct] # ignore punctuation
-        # append each vectorised sentence's values array to main array
-        vectorised_text.append(vectors)
-    return vectorised_text
-
-
+        doc = nlp(sentence) # treat sentence as a spaCy object
+        # convert sentence to a list of tokens
+        tokens = [token.text for token in doc if not token.is_punct] # ignore punctuation
+        tokenised_text.append(" ".join(tokens)) # add list of tokens to main list
+    return tokenised_text
 
 
 

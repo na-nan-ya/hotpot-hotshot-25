1a491fb125d52eb4fc30f03b5037aaedb2946d31
Ananya <ananyavganesh@gmail.com>
2025-02-22 23:49:51 -0500

    model class and functions defined + need to test on actual dataset

--- new_classifier.py
+++ new_classifier.py
@@ -23,7 +23,7 @@ def text2token(file_name: str):
     token_vectors = [token.vector for token in flat_tokens]
     return token_vectors
 
-def create_data_loaders(vector_list: list):
+def create_data_loaders(vector_list: list, train_or_test: str):
     vectorised_data = np.array(vector_list)
     label_array = np.array(generate_labels(dataset_size, "eng"))
     data_train, data_test, label_train, label_test \
@@ -39,6 +39,11 @@ def create_data_loaders(vector_list: list):
 
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    if train_or_test == "train":
+        return train_loader
+    else:
+        return test_loader
     
 
 def count(tokenList: list):
@@ -61,19 +66,50 @@ def generate_labels(num_items: int, first: str):
 class Mad_Hatter(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(Mad_Hatter, self).__init__()
-        
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, 2) 
+        self.fc_layer_1 = nn.Linear(input_size, hidden_size)
+        self.fc_layer_2 = nn.Linear(hidden_size, 2) 
         self.relu = nn.ReLU() 
 
-    def forward(self, x):
-        x = self.fc1(x)  
-        x = self.relu(x)  
-        x = self.fc2(x)  
-        return x  
+    def feedforward(self, input_batch):
+        input_batch = self.fc_layer_1(input_batch)  
+        input_batch = self.relu(input_batch)  
+        input_batch = self.fc_layer_2(input_batch)  
+        return input_batch  
 
+    def create_model(input_size, hidden_size):
+        model = Mad_Hatter(input_size, hidden_size)
+        loss_function = nn.BCEWithLogitsLoss()
+        optimizer = optim.Adam(model.parameters(), lr = 0.001)
+    
+    def train_binary_classification(model, data_loader, loss_f, optim, num_epochs = 10):
+        model.train()
+        for epoch in range(num_epochs):
+            loss_accumulation = 0.0 # track total loss for entire epoch
+            for data, labels in data_loader:
+                optim.zero_grad() # reset gradients of all parameters every iteration
+                outputs = model(data) # forward pass of input through model (produces raw output scores)
+                loss = loss_f(outputs.squeeze(), labels.float()) # compute binary loss between predicted outputs and true labels
+                loss.backward() # backward pass for autograd of each parameter
+                optim.step() # adjust learning rate
+                loss_accumulation += loss.item() # add loss value for current batch 
+            print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss_accumulation/len(data_loader):.4f}")
 
+    def evaluate_model(model, data_loader):
+        model.eval() # set model to evaluation mode
+        predictions = []
+        labels = []
 
+        with torch.no_grad():
+            for data, labels in data_loader:
+                output = model(data)
+                prob = torch.sigmoid(output)
+                predict = (prob > 0.5).float()
+                predictions.extend(predict.squeeze().tolist())  
+                labels.extend(labels.tolist())
+        return predictions, labels
+
+    #def print_results(predict_list, label_list)
+                
 
 
 
